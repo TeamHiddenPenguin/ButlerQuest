f51c6021f02e8da06ab4bcf20a88ffb19736d0ca
Samuel Sternklar <ssternklar@gmail.com>
2014-03-24 23:12:14 -0400

    Removed all dependencies on external code. All of the code, including the A* implementation, is now ours.

--- ButlerQuest/AStar.cs
+++ ButlerQuest/AStar.cs
@@ -1,5 +1,4 @@
-﻿//Code from http://blogs.msdn.com/b/ericlippert/archive/2007/10/10/path-finding-using-a-in-c-3-0-part-four.aspx
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,37 +9,6 @@ namespace ButlerQuest
 {
     class AStar
     {
-        /*static public Path<Node> FindPath<Node>(
-    Node start,
-    Node destination,
-    Func<Node, Node, double> distance,
-    Func<Node, double> estimate)
-    where Node : SquareGraphNode
-        {
-            if (estimate == null)
-                estimate = n => distance(n, destination);
-
-            var closed = new HashSet<Node>();
-            var queue = new PriorityQueue<double, Path<Node>>();
-            queue.Enqueue(0, new Path<Node>(start));
-            while (!queue.IsEmpty)
-            {
-                var path = queue.Dequeue();
-                if (closed.Contains(path.LastStep))
-                    continue;
-                if (path.LastStep.Equals(destination))
-                    return path;
-                closed.Add(path.LastStep);
-                foreach (Node n in path.LastStep.Neighbors)
-                {
-                    double d = distance(path.LastStep, n);
-                    var newPath = path.AddStep(n, d);
-                    queue.Enqueue(newPath.TotalCost + estimate(n), newPath);
-                }
-            }
-            return null;
-        }*/
-
         public static List<SquareGraphNode> FindPath(SquareGraphNode start, SquareGraphNode end, Func<SquareGraphNode, SquareGraphNode, double> distance, Func<SquareGraphNode, SquareGraphNode, double> estimate)
         {
             HashSet<SquareGraphNode> closedSet = new HashSet<SquareGraphNode>();
@@ -50,19 +18,19 @@ namespace ButlerQuest
             queue.Enqueue(0, initialPath);
             while (!queue.IsEmpty)
             {
-                var path = queue.Dequeue();
-                var currentNode = path.Last();
+                var currentPath = queue.Dequeue();
+                var currentNode = currentPath.Last();
                 if (closedSet.Contains(currentNode))
                     continue;
                 if (currentNode.Equals(end))
-                    return path;
+                    return currentPath;
                 closedSet.Add(currentNode);
                 foreach (var node in currentNode.Neighbors)
                 {
                     double dist = distance(currentNode, node);
-                    var newPath = new List<SquareGraphNode>(path);
+                    var newPath = new List<SquareGraphNode>(currentPath);
                     newPath.Add(node);
-                    queue.Enqueue(CalculatePathCost(path) + dist + estimate(node, end), newPath);
+                    queue.Enqueue(CalculatePathCost(currentPath) + dist + estimate(node, end), newPath);
                 }
             }
             return null;
@@ -88,32 +56,5 @@ namespace ButlerQuest
             return Math.Sqrt(Math.Pow(end.X - start.X, 2) + Math.Pow(end.Y - start.Y, 2));
         }
 
-    }
-    //Code from http://blogs.msdn.com/b/ericlippert/archive/2007/10/08/path-finding-using-a-in-c-3-0-part-three.aspx
-    class Path<Node> : IEnumerable<Node>
-    {
-        public Node LastStep { get; private set; }
-        public Path<Node> PreviousSteps { get; private set; }
-        public double TotalCost { get; private set; }
-        private Path(Node lastStep, Path<Node> previousSteps, double totalCost)
-        {
-            LastStep = lastStep;
-            PreviousSteps = previousSteps;
-            TotalCost = totalCost;
-        }
-        public Path(Node start) : this(start, null, 0) { }
-        public Path<Node> AddStep(Node step, double stepCost)
-        {
-            return new Path<Node>(step, this, TotalCost + stepCost);
-        }
-        public IEnumerator<Node> GetEnumerator()
-        {
-            for (Path<Node> p = this; p != null; p = p.PreviousSteps)
-                yield return p.LastStep;
-        }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
-        }
     }
 }
