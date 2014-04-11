abe810e3df5b44825e3e69ed8a5210dfdef4d88e
Drew Stanton <dms4052@rit.edu>
2014-04-11 11:37:05 -0400

    Added Coin and Weapon classes. Added GenerateWeapon and GenerateCoin methods in entity generator. Made some of the GameObject classes call base.Update because they weren't doing that before. Moved the Center variable from Enemy to GameObject. Gave money values to enemies. Gave Player an attack method that tells whether or not the given enemy collides with the player's weapon or not (NOT COMPLETE YET). Updated Level class to handle the new objects and everything associated with them.

---
+++ ButlerQuest/GameObject Hierarchy/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+// Drew Stanton
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ButlerQuest
+{
+    public class Weapon : DrawableGameObject
+    {
+        public int durability;
+        Vector3 attackLocation;
+        Rectangle attackRectangle;
+        public bool taken;
+
+        public Weapon(Animation[] animations, string[] names, Vector3 location, Rectangle rect, int durable, Vector3 attackLoc, Rectangle attackRect)
+            : base(animations, names, location, rect)
+        {
+            durability = durable;
+            attackLocation = attackLoc;
+            attackRectangle = attackRect;
+            taken = false;
+        }
+    }
+}
--- ButlerQuest/Level.cs
+++ ButlerQuest/Level.cs
@@ -16,6 +16,8 @@ namespace ButlerQuest
         public Player player; // the player. It's public so we can transfer info from one level to another (lives remaining, possibly score) without having to make the player in the gamestate management.
         List<Enemy> basicEnemies; // a list to hold all of the normal butlers for the level
         List<Wall> walls; // a list to hold all of the walls for the level
+        List<Weapon> weapons; // a list to hold all of the weapons for the level
+        List<Coin> coins; // a list to hold all of the coins for the level
         public Map levelMap; // parses the map and tiles used for the specific level
         public Rectangle windowSpace; // window space used for drawing the map
         GraphicsDevice graphics;
@@ -52,6 +54,14 @@ namespace ButlerQuest
                         {
                             walls.Add(EntityGenerator.GenerateWall(new Vector3(entity.X, entity.Y, currentFloor), entity.Width, entity.Height));
                         }
+                        else if (entity.Type == "Weapon")
+                        {
+                            weapons.Add(EntityGenerator.GenerateWeapon(new Vector3(entity.X, entity.Y, currentFloor), 2, entity.Name));
+                        }
+                        else if (entity.Type == "Coin")
+                        {
+                            coins.Add(EntityGenerator.GenerateCoin(new Vector3(entity.X, entity.Y, currentFloor), int.Parse(entity.Properties.Find(x => x.Item1 == "value").Item2)));
+                        }
                     }
                 }
                 //Otherwise it's a floor graph and sam will write this code later when it's relevant
@@ -72,7 +82,14 @@ namespace ButlerQuest
 
             levelMap.Draw(windowSpace, (int)player.location.Z);
 
-            foreach (Enemy enemy in basicEnemies) enemy.Draw(spriteBatch);
+            if (basicEnemies != null) foreach (Enemy enemy in basicEnemies) enemy.Draw(spriteBatch);
+
+
+            if (weapons != null) foreach (Weapon weapon in weapons) if (weapon.taken == false) weapon.Draw(spriteBatch);
+
+
+            if (coins != null) foreach (Coin coin in coins) if (coin.active) coin.Draw(spriteBatch);
+
 
             player.Draw(spriteBatch);
 
@@ -83,6 +100,9 @@ namespace ButlerQuest
         public void Update(GameTime gameTime)
         {
             player.Update(gameTime);
+
+            if (weapons != null) foreach (Weapon weapon in weapons) weapon.Update(gameTime);
+            if (weapons != null) foreach (Coin coin in coins) coin.Update(gameTime);
 
             AIManager.SharedAIManager.MakePaths();
 
@@ -125,6 +145,34 @@ namespace ButlerQuest
                     default: break;
                 }
             }
+
+            // coin collision
+            if (coins != null) foreach (Coin coin in coins)
+            {
+                int collision = player.CollisionSide(coin);
+                if (collision > -1)
+                {
+                    player.moneyCollected += coin.InteractWith();
+                }
+            }
+
+            // coin collision
+            if (player.currentWeapon == null)
+            {
+                if (weapons != null) foreach (Weapon weapon in weapons)
+                {
+                    if (weapon.taken == false)
+                    {
+                        int collision = player.CollisionSide(weapon);
+                        if (collision > -1)
+                        {
+                            player.currentWeapon = weapon;
+                            weapon.taken = true;
+                        }
+                    }
+                }
+            }
+
             windowSpace.X = (int)(player.location.X + (player.rectangle.Width / 2)) - (windowSpace.Width / 2);
             windowSpace.Y = (int)(player.location.Y + (player.rectangle.Height / 2)) - (windowSpace.Height / 2);
 
